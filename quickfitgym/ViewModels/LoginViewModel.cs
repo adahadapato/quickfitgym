12465853eb612f4eb05890f139b163e3552c1066
Adahada Enobong <adahadapato@gmail.com>
2020-08-20 12:39:19 +0100

    #0001

--- quickfitgym/ViewModels/LoginViewModel.cs
+++ quickfitgym/ViewModels/LoginViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace quickfitgym.ViewModels
@@ -12,8 +13,58 @@ namespace quickfitgym.ViewModels
 
         public LoginViewModel()
         {
-            LoginCommand = new Command(OnLoginClicked);
+            if (IsChecked)
+            {
+                Preferences.Get("Email", string.Empty);
+                Preferences.Get("Password", string.Empty);
+            }
+            //LoginCommand = new Command(OnLoginClicked);
         }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                SetProperty(ref _password, value);
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                SetProperty(ref _email, value);
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        private string _phone;
+        public string Phone
+        {
+            get { return _phone; }
+            set
+            {
+                SetProperty(ref _phone, value);
+                OnPropertyChanged(nameof(Phone));
+            }
+        }
+
+        private bool _IsChecked;
+        public bool IsChecked
+        {
+            get { return _IsChecked; }
+            set
+            {
+                SetProperty(ref _IsChecked, value);
+                OnPropertyChanged(nameof(IsChecked));
+            }
+        }
+
 
         private async void OnLoginClicked(object obj)
         {
