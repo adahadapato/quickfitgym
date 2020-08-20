476eb5230b01e7b8357e3932775728197daee901
Adahada Enobong <adahadapato@gmail.com>
2020-08-20 14:01:46 +0100

    #0002

--- quickfitgym/AppShell.xaml.cs
+++ quickfitgym/AppShell.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using quickfitgym.ViewModels;
 using quickfitgym.Views;
 using Xamarin.Forms;
@@ -15,5 +17,37 @@ namespace quickfitgym
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
+        Dictionary<string, Type> routes = new Dictionary<string, Type>();
+        public ICommand NavigateCommand => new Command(Navigate);
+        //public ICommand SettingsCommand => new Command(async () => await PushPage(new OptionsPage()));
+
+        private async Task PushPage(Page page)
+        {
+            await Shell.Current.Navigation.PushModalAsync(page);
+            Shell.Current.FlyoutIsPresented = false;
+        }
+
+        private async void Navigate(object route)
+        {
+            ShellNavigationState state = Shell.Current.CurrentState;
+            await Shell.Current.GoToAsync($"{state.Location}/{route.ToString()}");
+            Shell.Current.FlyoutIsPresented = false;
+        }
+
+        void RegistrRoutes()
+        {
+            //routes.Add("articles", typeof(ArticlesPage));
+            //routes.Add("photos", typeof(PhotosPage));
+            //routes.Add("classes", typeof(ClassesPage));
+            routes.Add("about", typeof(AboutPage));
+            //routes.Add("videos", typeof(VideosPage));
+            //routes.Add("admin", typeof(AdminPage));
+            //routes.Add("programmes", typeof(ProgramesPage));
+            routes.Add("logout", typeof(LoginPage));
+            foreach (var i in routes)
+            {
+                Routing.RegisterRoute(i.Key, i.Value);
+            }
+        }
     }
 }
---
+++ quickfitgym/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace quickfitgym.ViewModels
+{
+    public class HomePageViewModel:BaseViewModel
+    {
+        public HomePageViewModel()
+        {
+        }
+    }
+}
