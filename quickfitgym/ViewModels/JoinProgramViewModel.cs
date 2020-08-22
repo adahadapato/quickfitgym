414fec67b96c6bf22633b7dc016c68cfe52b02bd
Adahada Enobong <adahadapato@gmail.com>
2020-08-22 13:18:32 +0100

    #0004

---
+++ quickfitgym/ViewModels/JoinProgramViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using quickfitgym.Models;
+
+namespace quickfitgym.ViewModels
+{
+    public class JoinProgramViewModel:BaseViewModel
+    {
+        public ObservableCollection<ProgramSchedule> ScheduleCollection { get; private set; }
+        public JoinProgramViewModel(Program program)
+        {
+            ScheduleCollection = new ObservableCollection<ProgramSchedule>(program.ProgramSchedules);
+        }
+
+        public JoinProgramViewModel()
+        {
+        }
+    }
+}
---
+++ quickfitgym/Views/JoinProgramPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using quickfitgym.Models;
+using quickfitgym.ViewModels;
+using Xamarin.Forms;
+
+namespace quickfitgym.Views
+{
+    public partial class JoinProgramPage : ContentPage
+    {
+        public JoinProgramPage(Program program)
+        {
+            InitializeComponent();
+            BindingContext = new JoinProgramViewModel(program);
+        }
+    }
+}
