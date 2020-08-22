9584e27740e702b6a074f320c1cfca6d9fd5a5c6
Adahada Enobong <adahadapato@gmail.com>
2020-08-22 23:36:08 +0100

    #0004

---
+++ quickfitgym/Models/PaymentModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace quickfitgym.Models
+{
+    public class Payment
+    {
+        public string Username { get; set; }
+        public int Planid { get; set; }
+    }
+
+    public class PayementPlan
+    {
+        public int PlanId { get; set; }
+        public string Plan { get; set; }
+        public double StartAmount { get; set; }
+        public double Amount { get; set; }
+        public int Duration { get; set; }
+    }
+}
