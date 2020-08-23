a0f18d2de346c5cccd3a05737b8bcf019a20d9ae
Adahada Enobong <adahadapato@gmail.com>
2020-08-23 16:59:10 +0100

    #0043

--- quickfitgym.Android/obj/Debug/android/environment.x86_64.s
+++ quickfitgym.Android/obj/Debug/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"8523c16d-bdd1-4511-b307-a24b9b67e948"
+	.asciz	"abba9d4c-fea4-44a3-9aee-df7af71bb1d8"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
