c38f4844479cdac23d478f72a3816b29f3e6cf74
Adahada Enobong <adahadapato@gmail.com>
2020-08-26 22:20:30 +0100

    #0094

---
+++ quickfitgym/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using quickfitgym.Models;
+using quickfitgym.Services;
+
+namespace quickfitgym.ViewModels
+{
+    public class CustomerViewModel : BaseViewModel
+    {
+        public ObservableCollection<Members> MembersCollection { get; private set; }
+        public ObservableCollection<Members> TrainersCollection { get; private set; }
+        public CustomerViewModel()
+        {
+            MembersCollection = new ObservableCollection<Members>();
+            TrainersCollection = new ObservableCollection<Members>();
+            GetMembers();
+        }
+
+        private Members _selectedMember;
+        public Members SelectedMember
+        {
+            get { return _selectedMember; }
+            set
+            {
+                SetProperty(ref _selectedMember, value);
+                OnPropertyChanged(nameof(SelectedMember));
+            }
+        }
+
+        private async void GetMembers()
+        {
+            var members = await ApiService.GetMembers();
+            if (members != null)
+            {
+                members.ForEach(x => { x.RegistrationDate = x.JoinDate.ToString("dd/MM/yyyy");
+                    x.PhotoUrl = (string.IsNullOrWhiteSpace(x.PhotoUrl)) ? "NormalProfile.png" : x.PhotoUrl;
+                });
+                
+                foreach (var m in members)
+                {
+                    //if (string.IsNullOrWhiteSpace(m.PhotoUrl))
+                    //    m.PhotoUrl = "NormalProfile.png";
+
+                    //m.RegistrationDate = m.JoinDate.ToString("dd/MM/yyyy");
+                    
+                    var trainer = m.Roles.Any(x=> x=="Trainer" || x=="Admin");
+                    var member = m.Roles.Any(x => x == "Member");
+                   if(member)
+                      MembersCollection.Add(m);
+
+                    if (trainer)
+                        TrainersCollection.Add(m);
+                }
+            }
+        }
+    }
+}
